62345953a78b8db61b12fcb119e29441d850c5f3
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-03-19 20:05:26 +0000

    ✨ feat(1.1): add get functions
    1. declare get functions and add private variables in particle.hpp
    2. implement get functions in particle.cpp

--- include/particle.hpp
+++ include/particle.hpp
@@ -1,8 +1,18 @@
-class Particle {
-    public:
-        Particle(double in_mass);
-        double getMass() const;
+#include <Eigen/Core>
 
-    private:
-        double mass;
+class Particle
+{
+public:
+    Particle(double in_mass);
+    Particle(double in_mass, Eigen::Vector3d position, Eigen::Vector3d velocity, Eigen::Vector3d acceleration);
+    Eigen::Vector3d getPosition();
+    Eigen::Vector3d getVelocity();
+    Eigen::Vector3d getAcceleration();
+    const double getMass();
+
+private:
+    const double mass;
+    Eigen::Vector3d position;
+    Eigen::Vector3d velocity;
+    Eigen::Vector3d acceleration;
 };
--- src/particle.cpp
+++ src/particle.cpp
@@ -1,5 +1,33 @@
 #include "particle.hpp"
 #include <Eigen/Core>
+#include <cmath>
+
+Particle::Particle(double in_mass) : mass{in_mass}
+{
+}
+
+Particle::Particle(double in_mass, Eigen::Vector3d position, Eigen::Vector3d velocity, Eigen::Vector3d acceleration) : mass{in_mass}, position{position}, velocity{velocity}, acceleration{acceleration}
+{
+}
+
+Eigen::Vector3d Particle::getPosition()
+{
+    return this->position;
+}
+
+Eigen::Vector3d Particle::getVelocity()
+{
+    return this->velocity;
+}
+
+Eigen::Vector3d Particle::getAcceleration()
+{
+    return this->acceleration;
+}
+const double Particle::getMass()
+{
+    return this->mass;
+}
 
 Particle::Particle(double in_mass) :
     mass{in_mass}
