42b0ac6601f4abc29631b4ec9b6bed2efd794758
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-03-20 11:28:48 +0000

    🧪 test(1.2): add three unit tests for the gravitational force
    1. test 1: test the acceleration due to gravitational force between two particles
    2. test 2: test the acceleration due to gravitational force between one particle and a list of particles may or may not include the particle itself
    3. test 3: test the acceleration due to gravitational force between one particle and two equally sized particles with the opposite position

--- test/test.cpp
+++ test/test.cpp
@@ -62,4 +62,51 @@ TEST_CASE("Artificial acceleration where vec_acceleration = - vec_position.", "[
     REQUIRE(position_correctness);
     REQUIRE(velocity_correctness);
 }
+
+TEST_CASE("Gravitational force between two particles", "[Gravity]")
+{
+    Particle p1{1, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    Particle p2{5, {1, 1, 1}, {0, 0, 0}, {0, 0, 0}};
+
+    Eigen::Vector3d expected_acceleration{0.625, 0.625, 0.625};
+
+    bool acceleration_correctness = calcAcceleration(p1, p2, 1.0).isApprox(expected_acceleration, 0.01);
+    REQUIRE(acceleration_correctness);
+}
+
+TEST_CASE("Gravitational force due to a list of particles including itself)", "[Gravity]")
+{
+    Particle p0{10, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    Particle p1{1, {1, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    Particle p2{1, {0, 1, 0}, {0, 0, 0}, {0, 0, 0}};
+    Particle p3{1, {0, 0, 1}, {0, 0, 0}, {0, 0, 0}};
+    std::vector<Particle> p_list_included{p0, p1, p2, p3};
+    std::vector<Particle> p_list_excluded{p1, p2, p3};
+
+    p0.updateAcceleration(p_list_included);
+    Eigen::Vector3d expected_acceleration_included = p0.getAcceleration();
+
+    p0.setAcceleration({0, 0, 0});
+    p0.updateAcceleration(p_list_excluded);
+    Eigen::Vector3d expected_acceleration_excluded = p0.getAcceleration();
+
+    bool acceleration_correctness = expected_acceleration_included.isApprox(expected_acceleration_excluded, 1e-9);
+    REQUIRE(acceleration_correctness);
+}
+
+TEST_CASE("Gravitational force due to two opposite particles", "[Gravity]")
+{
+    // origin particle
+    Particle p0(10, {0, 0, 0}, {0, 0, 0}, {0, 0, 0});
+    // two opposite particles
+    Particle p1{1, {1, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    Particle p2{1, {-1, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    // list of particles
+    std::vector<Particle> p_list{p0, p1, p2};
+    // update acceleration of p0
+    p0.updateAcceleration(p_list);
+    // check if acceleration is zero
+    Eigen::Vector3d expected_acceleration{0, 0, 0};
+    bool acceleration_correctness = p0.getAcceleration().isApprox(expected_acceleration, 0.01);
+    REQUIRE(acceleration_correctness);
 }
