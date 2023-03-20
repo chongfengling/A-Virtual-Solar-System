a117a7d55c1d0a0cde86c93882ec98528c272051
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-03-20 21:51:40 +0000

    ✨ feat(1.3): add CLI app and declare the Solar System
    1. declare function initialize_Solar_System()
    2. declare function update_Solar_System()
    3. update .gitignore about debug config files

--- app/main.cpp
+++ app/main.cpp
@@ -2,15 +2,79 @@
 #include "/usr/include/eigen3/Eigen/Core"
 #include "CLI11.hpp"
 #include "/workspaces/PHAS0100/a-virtual-solar-system-chongfengling/include/particle.hpp"
+#include <random>
+
+std::vector<Particle> initialize_Solar_System()
+{
+    // All the particles are stored in a vector, in order of Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+    std::vector<Particle> p_list;
+    return p_list;
+}
+
+std::vector<Particle> update_Solar_System(std::vector<Particle> Solar_System, double dt, double total_time, int n_steps)
+{
+    return Solar_System;
+}
 
 int main(int argc, char **argv)
 {
     CLI::App app("Solar System");
+    // version flag
+    std::string version = "0.1.0";
+    app.set_version_flag("--version", version);
+    // delta time required
+    double dt;
+    app.add_option("--dt, --delta_time", dt, "Time step for simulation")->check(CLI::PositiveNumber);
+    // use either total time or number of steps
+    double total_time;
+    app.add_option("--tt, --total_time", total_time, "Total time for simulation")->check(CLI::PositiveNumber);
+    int n_steps;
+    app.add_option("--ns, --n_steps", n_steps, "Number of steps for simulation")->check(CLI::PositiveNumber);
+
     CLI11_PARSE(app, argc, argv);
-    Eigen::Vector3d v{1, 0, 0};
+
+    if (argc == 1)
+    {
+        std::cout << app.help() << std::endl;
+        return 0;
+    }
+
+    if (dt > 0)
+    {
+        if (total_time > 0 & n_steps <= 0)
+        {
+            n_steps = total_time / dt;
+        }
+        else if (total_time <= 0 & n_steps > 0)
+        {
+            total_time = n_steps * dt;
+        }
+        else
+        {
+            // std::count << std::string(dt);
+            printf("%f\n", dt);
+            printf("%f\n", total_time);
+            printf("%d\n", n_steps);
+            // std::count << total_time;
+            // std::count << n_steps;
+            std::cerr << "Error: arguments are unacceptable, please refer to the help information '-h'." << std::endl;
+            return 1;
+        }
+    }
+    else
+    {
+        std::cerr << "Error: ht is required, please refer to the help information '-h'." << std::endl;
+        return 1;
+    }
+
+    // std::vector<Particle> initial_solar_system;
+
+    std::vector<Particle> initial_solar_system = initialize_Solar_System();
+
+    std::vector<Particle> updated_solar_system = update_Solar_System(initial_solar_system, dt, total_time, n_steps);
 
     std::cout << "Hello World!\n"
-              << v << std::endl;
+              << std::endl;
 
     return 0;
 }
