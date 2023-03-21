31a1515f650d84bc2057a665cebe2a81f7cb6b14
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-03-21 12:36:46 +0000

    ✨ feat(1.3): add Solar System functions
    1. implement function initialize_Solar_System()
    2. implement function update_Solar_System()

--- src/particle.cpp
+++ src/particle.cpp
@@ -81,11 +81,48 @@ std::vector<Particle> initialize_Solar_System()
 {
     // All the particles are stored in a vector, in order of Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
     std::vector<Particle> p_list;
+    // vector of masses of planets, sun = 1
+    std::vector<double> masses{1. / 6023600, 1. / 408524, 1. / 332946.038, 1. / 3098710, 1. / 1047.55, 1. / 3499, 1. / 22962, 1. / 19352};
+    // vector of distances of planets, earth = 1
+    std::vector<double> distances{0.4, 0.7, 1, 1.5, 5.2, 9.5, 19.2, 30.1};
+    // vector of angles, randomly generated between 0 and 2pi with length 8 (the sun is set to be the origin and excluded).
+    std::vector<double> angles;
+    std::mt19937 rng_mt{std::random_device{}()};
+    std::uniform_real_distribution<double> distribution{0, 2 * M_PI};
+    for (int i = 0; i < 8; i++)
+    {
+        angles.push_back(distribution(rng_mt));
+    }
+    // create the sun and append to the vector
+    p_list.push_back(Particle(1, Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 0, 0), Eigen::Vector3d(0, 0, 0)));
+
+    // create planets and append to the vector
+    for (int i = 0; i < 8; i++)
+    {
+        p_list.push_back(
+            Particle(masses[i],
+                     Eigen::Vector3d(distances[i] * sin(angles[i]), distances[i] * cos(angles[i]), 0),
+                     Eigen::Vector3d(-cos(angles[i]) / sqrt(distances[0]), sin(angles[i]) / sqrt(distances[i]), 0),
+                     Eigen::Vector3d(0, 0, 0)));
+    }
 
     return p_list;
 }
 
 std::vector<Particle> update_Solar_System(std::vector<Particle> Solar_System, double dt, double total_time, int n_steps)
 {
+    for (int n = 0; n < n_steps; n++)
+    {
+        // update the gravitational acceleration of each body
+        for (int i = 0; i < Solar_System.size(); i++)
+        {
+            Solar_System[i].updateAcceleration(Solar_System, 1);
+        }
+        // update the position and velocity of each body
+        for (int j = 0; j < Solar_System.size(); j++)
+        {
+            Solar_System[j].update(dt);
+        }
+    }
     return Solar_System;
 }
